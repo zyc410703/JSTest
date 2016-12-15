8493ebee58398de3632c74d6a2843c4d09fa466b
zyc410703 <307730713@qq.com>
2016-12-15 10:49:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
 
-
-// defineClass('ViewController', {
-//            viewDidLoad: function() {
-//            self.ORIGviewDidLoad();
-//            var width = require('UIScreen').mainScreen().bounds().width
-//            var btn = require('UIButton').alloc().initWithFrame({x:0, y:100, width:width, height:50})
-//            btn.setTitle_forState('这个按钮是通过JSPatch动态添加上去的啊', 0)
-//            btn.addTarget_action_forControlEvents(self, "handleBtn:", 1 << 6)
-//            btn.setBackgroundColor(require('UIColor').redColor())
-//            self.view().addSubview(btn)
-//            },
-//        handleBtn: function(sender) {
-//        console.log('这是动态添加的按钮响应事件')
-//    },
-// })
 require('UITableViewCell');
 defineClass('ViewController', {
     viewDidLoad: function() {
@@ -22,9 +7,6 @@ defineClass('ViewController', {
             hello = [1,2,3,4];
             console.log(hello[2]);
     },
-//    tableView_numberOfRowsInSection: function(tableView, section) {
-//        return 150;
-//    },
     tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
         var cell = tableView.dequeueReusableCellWithIdentifier("cell")
         if (!cell) {
