c6cec63c781d2a8c70f9e72c48ef6186e582fb0b
zyc410703 <307730713@qq.com>
2016-12-15 15:47:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ defineClass('ViewController', {
             hello = [1,2,3,4];
             console.log(hello[2]);
     },
-//     tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
-//         var cell = tableView.dequeueReusableCellWithIdentifier("cell")
-//         if (!cell) {
-//            cell = require('UITableViewCell').alloc().initWithStyle_reuseIdentifier(3, "cell")
-//         }
-//         var arr = self.dataArray().toJS();
-//         cell.textLabel().setText(arr[indexPath.row()]);
-//         cell.detailTextLabel().setText('what');
-//         return cell;   
-//     },
+    tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        if (!cell) {
+           cell = require('UITableViewCell').alloc().initWithStyle_reuseIdentifier(3, "cell")
+        }
+        var arr = self.dataArray().toJS();
+        cell.textLabel().setText(arr[indexPath.row()]);
+        cell.detailTextLabel().setText('what');
+        return cell;   
+    },
     tableView_heightForRowAtIndexPath: function(tableView, indexPath) {
         return 64;
     },
